733763f542b6af4f6454facfce0fefd5b4d18732
Ethan Honeycutt <ethan.honeycutt@yahoo.com>
2016-04-23 15:13:29 -0500

    Added Bootstrap css and js

--- index.py
+++ index.py
@@ -2,8 +2,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def index():
    return render_template('index.html')
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', 5000, True)
+   app.run('0.0.0.0', 5000)
