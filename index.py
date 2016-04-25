6661fb8a42bf5e80b61a020acc4be9ab8380f314
Ethan Honeycutt <ethan.honeycutt@yahoo.com>
2016-04-24 19:50:11 -0500

    Content, POST and routing.

--- index.py
+++ index.py
@@ -1,9 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-   return render_template('index.html')
+	return render_template('index.html')
+
+@app.route('/functions')
+def functions():
+	return render_template('functions.html')
+
+@app.route('/odl')
+def odl():
+	return render_template('odl.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
+@app.route('/blacklist', methods=['POST'])
+def blacklist():
+	print request.form['ip']
+	#requests.post('http://httpbin.org/post', data = {'key':request.form['ip']})
+	return 'Submitted IP ' + request.form['ip'];
 
 if __name__ == '__main__':
-   app.run('0.0.0.0', 5000)
+	app.run('0.0.0.0', 5000, True)
